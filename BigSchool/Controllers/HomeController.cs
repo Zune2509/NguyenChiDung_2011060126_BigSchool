6bcb060d22dba7faba1eb34f65c9e2222f889371
Zune <ngchidung2105@gmail.com>
2023-03-31 00:46:15 +0700

    Hoàn thành chức năng tuần 3

--- BigSchool/Controllers/HomeController.cs
+++ BigSchool/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
+using BigSchool.ViewModels;
 
 namespace BigSchool.Controllers
 {
@@ -22,7 +23,14 @@ namespace BigSchool.Controllers
                 .Include(c => c.Lecturer)
                 .Include(c => c.Category)
                 .Where(c => c.DateTime > DateTime.Now);
-            return View(upcomingCourses);
+
+            var viewModel = new CoursesViewModel
+            {
+                UpcomingCourses = upcomingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult About()
